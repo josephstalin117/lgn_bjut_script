db1edd3df76b454ebf9c92427d43e14a40c06de8
josephlin <josephlin117@hotmail.com>
2018-08-16 19:45:50 +0800

    edit gno code

--- login_bjut.sh
+++ login_bjut.sh
@@ -14,7 +14,7 @@ gno=$(echo "$content"|grep -o '^Gno=[0-9][0-9]')
 #echo ${msg}
 #echo ${gno}
 
-if [ "$gno" == "Gno=32" ]; then
+if [ "$gno" == "Gno=01" ]; then
     echo "login_success"
 else
     echo "first login fail"
@@ -24,7 +24,7 @@ else
     msg2=$(echo "$content2"|grep -o '^Msg=[0-9][0-9]')
     gno2=$(echo "$content2"|grep -o '^Gno=[0-9][0-9]')
 
-    if [ "$gno2" == "Gno=32" ]; then
+    if [ "$gno2" == "Gno=01" ]; then
         echo "login_success"
         echo ${gno2}
     else
