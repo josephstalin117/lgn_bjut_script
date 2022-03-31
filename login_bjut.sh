8eb0e67d70005beecb35ba2db75b216a8af5480f
josephlin <josephlin117@gmail.com>
2022-03-31 18:28:33 +0800

    update ipser

--- login_bjut.sh
+++ login_bjut.sh
@@ -7,8 +7,9 @@ upass2=
 DDDDD3=
 upass3=
 
+
 # 0 ipv4&ipv6 1 ipv4 2 ipv6
-v46s=0
+v46s=2
 
 if [ "$1" == 6 ]; then
     echo "ipv6"
@@ -32,21 +33,23 @@ fi
 
 #curl ${lgn}
 
+f4serip=172.30.201.10
 #content=$(curl -s 'https://lgn6.bjut.edu.cn/' --data 'DDDDD='${DDDDD}'&upass='${upass}'&v46s='${v46s}'&v6ip=&f4serip=172.30.201.2&0MKKey=')
-content=$(curl -s ${lgn} --data 'DDDDD='${DDDDD}'&upass='${upass}'&v46s='${v46s}'&v6ip=&f4serip=172.30.201.2&0MKKey=')
+content=$(curl -s ${lgn} --data 'DDDDD='${DDDDD}'&upass='${upass}'&v46s='${v46s}'&v6ip=&f4serip=${f4serip}&0MKKey=')
 
 #echo "${content}"
 
 msg=$(echo "$content"|grep -o '^Msg=[0-9][0-9]')
 gno=$(echo "$content"|grep -o '^Gno=[0-9][0-9]')
 
-if [ "$gno" == "Gno=01" ]; then
+#if [ "$gno" == "Gno=01" ]; then
+if [ "$gno" == "Gno=44" ]; then
     echo "login_success"
 else
     echo "first login fail"
     echo ${msg}
     echo ${gno}
-    content2=$(curl -s ${lgn} --data 'DDDDD='${DDDDD2}'&upass='${upass2}'&v46s='${v46s}'&v6ip=&f4serip=172.30.201.2&0MKKey=')
+    content2=$(curl -s ${lgn} --data 'DDDDD='${DDDDD2}'&upass='${upass2}'&v46s='${v46s}'&v6ip=&f4serip=${f4serip}&0MKKey=')
     msg2=$(echo "$content2"|grep -o '^Msg=[0-9][0-9]')
     gno2=$(echo "$content2"|grep -o '^Gno=[0-9][0-9]')
 
@@ -56,7 +59,7 @@ else
         echo ${msg2}
         echo ${gno2}
         echo "login_fail"
-        content3=$(curl -s ${lgn} --data 'DDDDD='${DDDDD3}'&upass='${upass3}'&v46s='${v46s}'&v6ip=&f4serip=172.30.201.2&0MKKey=')
+        content3=$(curl -s ${lgn} --data 'DDDDD='${DDDDD3}'&upass='${upass3}'&v46s='${v46s}'&v6ip=&f4serip=${f4serip}&0MKKey=')
         msg3=$(echo "$content3"|grep -o '^Msg=[0-9][0-9]')
         gno3=$(echo "$content3"|grep -o '^Gno=[0-9][0-9]')
         if [ "$gno3" == "Gno=01" ]; then
@@ -68,3 +71,4 @@ else
         fi
     fi
 fi
+
